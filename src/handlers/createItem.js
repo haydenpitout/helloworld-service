7f24998d8492d2e1e389a04a2512b71ab5e96ec0
Hayden <hayden.pitout@gmail.com>
2021-08-15 09:58:21 +1000

    Added database persistence for DynamoDB PutItem along with the associated IAM Role

--- src/handlers/createItem.js
+++ src/handlers/createItem.js
@@ -1,13 +1,24 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 export const handler = async (event, context) => {
   const { title, description } = JSON.parse(event.body);
   const now = new Date();
 
   const item = {
+    id: uuid(),
     title,
     description,
     status: 'OPEN',
     createdAt: now.toISOString(),
   };
+
+  await dynamodb.put({
+    TableName: 'ItemsTable',
+    Item: item,
+  }).promise();
 
   return {
     statusCode: 201,
