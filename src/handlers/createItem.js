a78291c5ab035698972689ebff08e37ad333ff5d
Hayden <hayden.pitout@gmail.com>
2021-08-15 17:36:00 +1000

    Added Middy middleware to provide middleware support, as well http-errors to improve the error handling

--- src/handlers/createItem.js
+++ src/handlers/createItem.js
@@ -1,10 +1,15 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-export const handler = async (event, context) => {
-  const { title, description } = JSON.parse(event.body);
+async function createItem(event, context) {
+  const { title, description } = event.body;
   const now = new Date();
 
   const item = {
@@ -15,13 +20,25 @@ export const handler = async (event, context) => {
     createdAt: now.toISOString(),
   };
 
-  await dynamodb.put({
-    TableName: 'ItemsTable',
-    Item: item,
-  }).promise();
+  try {
+    await dynamodb
+      .put({
+        TableName: process.env.ITEMS_TABLE_NAME,
+        Item: item,
+      })
+      .promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
     body: JSON.stringify({ item }),
   };
-};
+}
+
+export const handler = middy(createItem)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
